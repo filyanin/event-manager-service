62f7a37697702e9f1484ac5b221d2f506cce4d61
Compuhter\Compuhter <famely.fsa@yandex.ru>
2026-03-24 09:05:44 +0300

    Добавлен минимальный middleware без логирования

---
+++ EventManagerService/Infrastructure/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace EventManagerService.Infrastructure
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+        public ExceptionHandlingMiddleware(
+            RequestDelegate next,
+            ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex) 
+            {
+                await HandleException(context, ex);
+            }
+        }
+
+        public async Task HandleException (HttpContext context, Exception ex)
+        {
+            throw new NotImplementedException("logging");
+
+            if (context.Response.HasStarted)
+            {
+                return;
+            }
+            var stausCode = MapStatusCode(ex);
+
+            context.Response.StatusCode = stausCode;
+            context.Response.ContentType = "application/json";
+
+            var error = new ProblemDetails
+            {
+                Status = stausCode,
+                Detail = ex.Message
+            };
+
+            await context.Response.WriteAsJsonAsync(error);
+        }
+
+        private static int MapStatusCode(Exception ex) => ex switch
+        {
+            ValidationException ve => StatusCodes.Status400BadRequest,
+            _ => StatusCodes.Status500InternalServerError
+
+        };
+    }
+}
