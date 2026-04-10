aeead8b0bd821aaf274faad18c9c4a0e58357b3c
Compuhter\Compuhter <famely.fsa@yandex.ru>
2026-04-10 17:13:12 +0300

    Начало работы над booking service

---
+++ EventManagerService/Domain/Interfaces/BookingService/IBookingService.cs
@@ -0,0 +1,12 @@
+﻿using EventManagerService.Domain.Models.Booking;
+
+namespace EventManagerService.Domain.Interfaces.BookingService
+{
+    public interface IBookingService
+    {
+
+        public Booking CreateBookingAsync(Guid eventId);
+
+        public Booking GetBookingByIdAsync(Guid bookingId);
+    }
+}
---
+++ EventManagerService/Domain/Services/BookingService/BookingService.cs
@@ -0,0 +1,20 @@
+﻿using EventManagerService.Domain.Interfaces.BookingService;
+using EventManagerService.Domain.Models.Booking;
+
+namespace EventManagerService.Domain.Services.BookingService
+{
+    public class BookingService : IBookingService
+    {
+        private List<Booking> bookings = new List<Booking>();
+
+        public Booking CreateBookingAsync(Guid eventId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Booking GetBookingByIdAsync(Guid bookingId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
