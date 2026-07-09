2eb0c6c71e9b3d3adf435cfe5734412937581099
Никита Филянин <famely.FSA@yandex.ru>
2026-07-09 20:22:24 +0300

    Добавление пакетов, переход на миграции

--- EventManagerService/Program.cs
+++ EventManagerService/Program.cs
@@ -4,6 +4,7 @@ using EventManagerService.Domain;
 using EventManagerService.Infrastructure;
 using EventManagerService.Presentation;
 using EventManagerService.Infrastructure.DataAssets;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -18,7 +19,7 @@ var app = builder.Build();
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-    db.Database.EnsureCreated();
+    db.Database.Migrate();
 }
 
 // Configure the HTTP request pipeline.
